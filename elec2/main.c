115842172d29c10ca72784b98492cfdca153aaef
toby <tobygeneva@hotmail.com>
2024-10-21 12:04:28 +0200

    Initial Commit

---
+++ elec2/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  elec2
+//
+//  Created by Toby Simpson on 21.10.2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
